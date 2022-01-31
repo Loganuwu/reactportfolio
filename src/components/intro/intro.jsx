f2fbbca00709ac595b5c29ffad674d84f0dc15b7
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2022-01-31 08:03:38 -0800

    flex box

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,12 @@
+import "./intro.css"
+
+const Intro = () => {
+  return <div className="i">
+      <div className="i-left">Left</div>
+      <div className="i-right">Right</div>
+  </div>;
+};
+
+export default Intro;
+
+
