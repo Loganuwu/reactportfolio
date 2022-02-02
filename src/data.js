a092000526b9e26ba241e657ec5583150f8659a6
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2022-02-02 15:12:10 -0800

    updated img

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ export const featuredPortfolio = [
       id: 3,
       title: "News-Finder",
       img:
-        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+        "./assets/newsFinder.png",
     },
 
   ];
