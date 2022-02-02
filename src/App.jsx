ad1ac153deee1e022a11a8ee96b6b6aa35898a6e
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2022-02-02 14:34:54 -0800

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/Works"
+import Contact from "./components/contact/Contact"
+import "./app.scss"
+import { useState } from "react";
 import React from "react";
+import Menu from "./components/menu/Menu";
 
-const App = () => {
-  return <div className="app">
-    top
-    sec
-  </div>;
-};
+function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
+  return (
+    <div className="app">
+     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <div className="sections">
+       <Intro/>
+       <Portfolio/>
+       <Works/>
+       <Contact/>
+     </div>
+    </div>
+  );
+}
 
 export default App;
